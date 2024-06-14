5108756b4ff0e6e68e67d81b570fbc450608e180
vinicius <viniwarrior4@gmail.com>
2024-06-14 15:49:14 -0300

    typing for new libsignal

--- src/Types/Auth.ts
+++ src/Types/Auth.ts
@@ -1,3 +1,4 @@
+import { SessionRecordList } from 'libsignal'
 import type { proto } from '../../WAProto'
 import { RegistrationOptions } from '../Socket/registration'
 import type { Contact } from './Contact'
@@ -73,7 +74,7 @@ export type AuthenticationCreds = SignalCreds & {
 
 export type SignalDataTypeMap = {
     'pre-key': KeyPair
-    'session': Uint8Array
+    'session': SessionRecordList
     'sender-key': Uint8Array
     'sender-key-memory': { [jid: string]: boolean }
     'app-state-sync-key': proto.Message.IAppStateSyncKeyData
